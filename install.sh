f0493e51c7f1dcc604a7bbf6b1e77c7bd6ac01d2
Patrick Hereford <phereford@users.noreply.github.com>
2017-02-25 09:50:26 -0500

    Added an install script.

---
+++ install.sh
@@ -0,0 +1,13 @@
+cd
+git clone git@github.com:phereford/dotfiles.git ~/.dotfiles
+cd .dotfiles
+
+ln -s .vimrc ~/.vimrc
+ln -s .ctags ~/.ctags
+ln -s .zshrc ~/.zshrc
+
+mkdir ~/.vim
+git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundles
+source ~/.vimrc
+source ~/.zshrc
+vim +PluginInstall +qall
