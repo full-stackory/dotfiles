1f262c34725eb6116c768fc7bbc704e0b8ec1a7c
Patrick Hereford <phereford@users.noreply.github.com>
2017-10-21 13:22:42 -0400

    Added base code styles for vim linting. Updated install script

--- install.sh
+++ install.sh
@@ -7,6 +7,10 @@ cd .dotfiles
 ln -s .vimrc ~/.vimrc
 ln -s .ctags ~/.ctags
 ln -s .zshrc ~/.zshrc
+ln -s ~/dotfiles/code_styles/rubocop.yml ~/.rubocop.yml
+ln -s ~/dotfiles/code_styles/eslintrc.json ~/.eslintrc.json
+ln -s ~/dotfiles/code_styles/scss-lint.yml ~/.scss-lint.yml
+ln -s ~/dotfiles/code_styles/credo.exs ~/.credo.exs
 
 mkdir ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundles
